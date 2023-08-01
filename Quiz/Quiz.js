7a8c878c4e3138b9f59235e33b5159694a909dc0
dababyletgo <belloolajide1@gmail.com>
2023-08-02 00:33:46 +0100

    lol

---
+++ Quiz/Quiz.js
@@ -0,0 +1,34 @@
+function changeColor() { 
+    var inputtedText = document.getElementById("myText").value
+    var inputtedlText = document.getElementById("mynText").value
+    var inputtedkText = document.getElementById("mybText").value
+    var inputtedjText = document.getElementById("myvText").value
+    //alert(inputtedText);
+    if(inputtedText == "Washington"){
+    document.getElementById("Question1").style.color = "green";
+    }
+    else{
+        document.getElementById("Question1").style.color = "red"; 
+    }
+
+     if(inputtedlText == "Whale"){
+        document.getElementById("Question2").style.color = "green";
+        }
+        else{
+            document.getElementById("Question2").style.color = "red"; 
+        }
+
+    if(inputtedkText == "Joe Biden"){
+        document.getElementById("Question3").style.color = "green";
+        }
+        else{
+            document.getElementById("Question3").style.color = "red"; 
+        }
+
+    if(inputtedjText == "Tinubu"){
+        document.getElementById("Question4").style.color = "green";
+        }
+        else{
+            document.getElementById("Question4").style.color = "red"; 
+        }
+}
