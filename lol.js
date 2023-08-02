b61278cc7979fc50d269054ad2a72e7bb5814c97
dababyletgo <belloolajide1@gmail.com>
2023-08-02 20:10:44 +0100

    lol

--- lol.js
+++ lol.js
@@ -1,1 +1,7 @@
-alert('hello')
+function backgroundcolor() {
+var color = document.getElementsByClassName("Container1").value
+var button = document.getElementsById("myColorChanger").value
+ if(l){
+    document.getElementsByClassName("Container1").style.color = "green";
+ }
+}
