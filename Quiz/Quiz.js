059b13f3057146086568fd887040fbe8ff7af21f
dababyletgo <belloolajide1@gmail.com>
2023-08-04 18:17:11 +0100

    fixed lol bugs

--- Quiz/Quiz.js
+++ Quiz/Quiz.js
@@ -1,14 +1,17 @@
 function changeColor() { 
-    var inputtedText = document.getElementById("myText").value
+    var inputtedText1 = document.getElementById("myText1").value
+    var inputtedText2 = document.getElementById("myText2").value
+    var inputtedText3 = document.getElementById("myText3").value
+    var inputtedText4 = document.getElementById("myText4").value
         //alert(inputtedText);
-    if(inputtedText == "Washington"){
+    if(inputtedText1 == "Washington"){
     document.getElementById("Question1").style.color = "green";
     }
     else{
         document.getElementById("Question1").style.color = "red"; 
     }
 
-     if(inputtedText == "Whale"){
+     if(inputtedText2 == "Whale"){
         document.getElementById("Question2").style.color = "green";
         }
         else{
@@ -16,14 +19,14 @@ function changeColor() {
         }
 
   
-        if(inputtedText == "Joe Biden"){
+        if(inputtedText3 == "Joe Biden"){
         document.getElementById("Question3").style.color = "green";
         }
         else{
             document.getElementById("Question3").style.color = "red"; 
         }
 
-        if(inputtedText == "Tinubu"){
+        if(inputtedText4 == "Tinubu"){
         document.getElementById("Question4").style.color = "green";
         }
         else{
