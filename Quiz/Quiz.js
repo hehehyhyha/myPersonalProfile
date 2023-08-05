b5332fac5527818d65695b896c47e8a35e16b0a3
dababyletgo <belloolajide1@gmail.com>
2023-08-05 12:22:10 +0100

    Added alert

--- Quiz/Quiz.js
+++ Quiz/Quiz.js
@@ -3,33 +3,40 @@ function changeColor() {
     var inputtedText2 = document.getElementById("myText2").value
     var inputtedText3 = document.getElementById("myText3").value
     var inputtedText4 = document.getElementById("myText4").value
-        //alert(inputtedText);
     if(inputtedText1 == "Washington"){
-    document.getElementById("Question1").style.color = "green";
+    document.getElementById("Question1").style.color = "green"
+    alert("You go it Right");
     }
     else{
-        document.getElementById("Question1").style.color = "red"; 
+        document.getElementById("Question1").style.color = "red"
+        alert("Wrong it was Washington");
     }
 
      if(inputtedText2 == "Whale"){
-        document.getElementById("Question2").style.color = "green";
+        document.getElementById("Question2").style.color = "green"
+        alert("You go it Right");
         }
         else{
-            document.getElementById("Question2").style.color = "red"; 
+            document.getElementById("Question2").style.color = "red"
+            alert("Wrong it was Whale");
         }
 
   
         if(inputtedText3 == "Joe Biden"){
-        document.getElementById("Question3").style.color = "green";
+        document.getElementById("Question3").style.color = "green"
+        alert("You go it Right");
         }
         else{
-            document.getElementById("Question3").style.color = "red"; 
+            document.getElementById("Question3").style.color = "red"
+            alert("Wrong it was Joe Biden");
         }
 
         if(inputtedText4 == "Tinubu"){
-        document.getElementById("Question4").style.color = "green";
+        document.getElementById("Question4").style.color = "green"
+        alert("You go it Right");
         }
         else{
-            document.getElementById("Question4").style.color = "red"; 
+            document.getElementById("Question4").style.color = "red"
+            alert("Wrong it was Tinubu");
         }
 }
