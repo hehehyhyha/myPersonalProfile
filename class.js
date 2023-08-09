0a4d2886b1726a0ce085791707c2ffc0356ac2a6
dababyletgo <belloolajide1@gmail.com>
2023-08-09 20:52:20 +0100

    made folder

--- class.js
+++ class.js
@@ -1,3 +1,8 @@
-//function lol (number){
-    //if(number > 5)
-//}
+function Testingif (number){
+    if(number > 5) {
+        console.log("This number is greater than 5")
+    }
+    else {
+        console.log("less than 5")
+    }
+}
